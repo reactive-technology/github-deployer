f3894bc53fd83eae5e33cbd2b51989aca804da92
Benamar Belarbi <benamarbelarbi@yahoo.fr>
2019-04-12 15:11:33 +0200

    add multi target/repo deployer,  add variables to manage version deployments

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
-
+require("dotenv").config({path: __dirname + "/.env"});
+var wildcard = require('wildcard');
 
 var express = require('express'),
     app = express(),
@@ -18,31 +19,54 @@ verifyGitHubSignature.setSecret(secret);
 
 getConfig(function (config) {
     deployTasks.initConfig(config);
-
-    var branch = config.branch || 'master';
-
+    if (config.branch && !config.projects) {
+        config.projects = [config];
+    }
     app.post(config.route, function (req, res) {
 
-        // Checking if request is authentic
-        if (verifyGitHubSignature.ofRequest(req)) {
+        for (const projectId in config.projects) {
+            const project = config[projectId];
+            const cf_branch = project.branch || config.branch || 'master';
+            const cfTagSearch = project.tagsearch || config.tagsearch || undefined;
+            const cfRepo = project.repository_name || config.repository_name || undefined;
+            // Checking if request is authentic
+            if (req.body
+                && req.body.ref
+                && req.body.repository
+                && req.body.pusher
+                && verifyGitHubSignature.ofRequest(req)) {
+                // If master was updated, do stuff
+                const tag = req.body.ref.replace('refs/tags/', '');
+                const branch = req.body.ref.replace('refs/heads/', '');
+                const repository_name = req.body.repository.name;
+                const repository_ssh_url = req.body.repository.ssh_url;
+                const pusher_email = req.body.pusher.email;
+                const params = {tag, branch, repository_name, repository_ssh_url, pusher_email};
+                if (req.body.ref && wildcard('refs/tags/' + cfTagSearch, req.body.ref) && req.body.created) {
 
-            // If master was updated, do stuff
-            if (req.body.ref && req.body.ref === `refs/heads/${branch}`) {
-
-                console.log('Valid payload! Running commands');
-
-                deployTasks.run(function () {
-                    res.status(200).send();
-                });
-
+                    if (req.body.ref && req.body.ref.indexOf(`refs/tags/${cfTagSearch}`) === 0 && req.body.created) {
+                        console.log('Valid tag payload! Running commands');
+                        deployTasks.run(function () {
+                            res.status(200).send();
+                        }, params);
+                    } else if (req.body.ref && req.body.ref === `refs/heads/${cf_branch}`) {
+                        console.log('Valid branch payload! Running commands');
+                        deployTasks.run(function () {
+                            res.status(200).send();
+                        }, params);
+                    } else {
+                        // if other branches were updated, send 200 only to make github happy...
+                        cfTagSearch && console.log(`Received tag payload unrelated to  ${cfTagSearch} tag`);
+                        cf_branch && console.log(`Received branch payload unrelated to ${cf_branch} branch`);
+                        res.status(200).send();
+                    }
+                } else {
+                    console.log(`the received repository ${repository_name} does not match ${cfRepo}`);
+                }
             } else {
-                // if other branches were updated, send 200 only to make github happy...
-                console.log(`Received payload unrelated to ${branch} branch`);
-                res.status(200).send();
+                console.warn('Received payload with an invalid secret');
+                res.status(403).send();
             }
-        } else {
-            console.warn('Received payload with an invalid secret');
-            res.status(403).send();
         }
     });
 
