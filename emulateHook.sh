9f0b85ce70def5b9b6757066d3f069c3ebdf6d91
Benamar Belarbi <benamarbelarbi@yahoo.fr>
2019-04-24 11:48:29 +0200

    create hook emulator for test purpose

--- emulateHook.sh
+++ emulateHook.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-
-  data=$(cat /tmp/github.json)
+  FILE=/tmp/github.json
+  WEBHOOK_PATH=$(grep route config.json |cut -f4 -d'"')
+  data=$(cat $FILE)
 
   sig=$(echo -n "${data}" | openssl dgst -sha1 -hmac "%{WEBHOOK_SECRET}" | awk '{print "X-Hub-Signature: sha1="$2}')
-
+   echo calling http://localhost:5000${WEBHOOK_PATH} with $FILE github hook simulate content
   curl -X POST -H "Content-Type: application/json" -H "${sig}" -d "${data}" http://localhost:5000${WEBHOOK_PATH}
 
